14305a92bcb0821f3cb85ace8d10b813c3695da4
Felipe Bittencourt <felipebittr@hotmail.com>
2018-01-02 16:04:05 -0200

    Initial Commit

---
+++ src/reducers/SelectionReducer.js
@@ -0,0 +1,10 @@
+export default (state = null, action) => {
+    console.log(action);
+    console.log(state);
+    switch (action.type) {
+        case 'select_library':
+         return action.payload;
+        default:
+         return state;
+    }
+};
